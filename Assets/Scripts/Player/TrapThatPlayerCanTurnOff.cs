a3cfe8dfeb6217de14655281b25a81702bc521ed
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-04-10 22:37:19 +0100

    Add new Trap Script

--- Assets/Scripts/Player/TrapThatPlayerCanTurnOff.cs
+++ Assets/Scripts/Player/TrapThatPlayerCanTurnOff.cs
@@ -1,16 +1,32 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TrapThatPlayerCanTurnOff : MonoBehaviour 
 {
+    private bool trapAcitvate = false;
+    private Player player;
+    private Collider2D trapCollider;
+    private Collider2D playerCollision;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = FindFirstObjectByType<Player>();
+        playerCollision = player.GetComponent<Collider2D>();
+        trapCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.)
+        {
+            Debug.Log("OPA");
+        }
     }
 }
