8b483301416243be6020498ff7cf05d1391cde53
unknown <goncalonascimento.gamedesign@gmail.com>
2025-04-14 19:37:25 +0100

    Changes in the way that beartrap works

--- Assets/Scripts/Player/TrapThatPlayerCanTurnOff.cs
+++ Assets/Scripts/Player/TrapThatPlayerCanTurnOff.cs
@@ -6,10 +6,10 @@ using UnityEditor.Experimental.GraphView;
 public class TrapThatPlayerCanTurnOff : MonoBehaviour 
 {
     [SerializeField] private int damage = 1; 
-    private void OnTriggerEnter2D(Collider2D collider)
+    private void OnTriggerStay2D(Collider2D collider)
     {
         HealthSystem healthSystem = collider.GetComponentInParent<HealthSystem>();
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift) || healthSystem.transform.position == healthSystem.transform.position)
         {
             Debug.Log($"The {name} is deactivated");
         }
