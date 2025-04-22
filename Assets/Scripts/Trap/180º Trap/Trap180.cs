625874b9507f4b473001dc184020a1c078af9536
unknown <goncalonascimento.gamedesign@gmail.com>
2025-04-22 03:25:44 +0100

    Add rotation to the trap, does not stop yet

--- Assets/Scripts/Trap/180º Trap/Trap180.cs
+++ Assets/Scripts/Trap/180º Trap/Trap180.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
@@ -6,19 +7,29 @@ public class Trap180 : MonoBehaviour
     [SerializeField]
     private Trigger180 trigger180;
     [SerializeField]
-    private float LerpChange = 0.1f;
+    private float lerpChange = 0.1f;
+    [SerializeField]    
+    private float rotateSpeed;
+    [SerializeField, Description("0 - 360")]
+    private float angleToStop = 270;
     
-    private float Rotation;
+    private float rotation;
 
 
     private void Update()
     {
-        
         if (trigger180.IsActivated() == true)
         { 
-            for (float i = 0; i > -90; i--)
+            rotation += Time.deltaTime * rotateSpeed;
+            if (transform.eulerAngles.z != angleToStop)
             {
-                gameObject.transform.rotation = Quaternion.Euler(0, 0, i*Time.deltaTime);
+                gameObject.transform.rotation = Quaternion.Euler(0, 0, -rotation);
+                Debug.Log(transform.eulerAngles.z);    
+            }
+            else 
+            {
+                gameObject.transform.rotation = Quaternion.Euler(0, 0, -90f);
+                Debug.Log("POR FAVOR PARA");
             }
         }
     }
