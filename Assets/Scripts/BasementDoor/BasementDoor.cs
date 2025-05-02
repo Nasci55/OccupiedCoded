3e23faa5892b21ee100132495aec8baa67a97e46
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-05-02 15:56:28 +0100

    Add code for Lock in the basement Door

--- Assets/Scripts/BasementDoor/BasementDoor.cs
+++ Assets/Scripts/BasementDoor/BasementDoor.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class BasementDoor : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private BasementLock isUnlocked;
+    private bool isPlayerInside;
+    private Player player;
+    private Camera camera;
+
+    private void OnTriggerEnter2D(Collider2D collider)
     {
+        isPlayerInside = true;
+        Debug.Log("Player entered the area");
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        isPlayerInside = false;
+        Debug.Log("Player left the area");
+    }
+
+    private void Start()
+    {
+        player = FindFirstObjectByType<Player>();
+        camera = FindFirstObjectByType<Camera>();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+        if (isPlayerInside && Input.GetKeyDown(KeyCode.W))
+        {
+            
+        }
     }
 }
--- Assets/Scripts/BasementDoor/BasementLock.cs
+++ Assets/Scripts/BasementDoor/BasementLock.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class BasementLock : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private bool IsLocked = false;
+    private Player player;
+
     void Start()
     {
-        
+        player = FindFirstObjectByType<Player>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collider)
     {
-        
+        player = collider.GetComponentInParent<Player>();
+        if (player != null)
+        {
+            IsLocked = true;
+            Destroy(gameObject.GetComponent<SpriteRenderer>());
+        }
     }
+
+    public bool GeIsLocked()
+    { return IsLocked; }
+
+
 }
