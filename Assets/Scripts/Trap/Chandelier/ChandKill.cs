d9f6aa99fa9e5a60ba4f42ad67e5bbfd5bbf62d2
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-05-05 18:44:03 +0100

    Add Sound to the chandelier, removed some traps

--- Assets/Scripts/Trap/Chandelier/ChandKill.cs
+++ Assets/Scripts/Trap/Chandelier/ChandKill.cs
@@ -5,23 +5,27 @@ public class ChandKill : MonoBehaviour
     [SerializeField] private int damage = 1;
     [SerializeField]
     private AudioClip audioClip;
+    [SerializeField] 
+    private AudioClip audioClipHitting;
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         HealthSystem healthSystem = collider.GetComponentInParent<HealthSystem>();
         if (healthSystem == null)
         {
-            SoundManager.instance.playSound(audioClip, transform, 0.1f);
+            SoundManager.instance.playSound(audioClip, transform, 0.07f);
             Destroy(gameObject.GetComponent<BoxCollider2D>());
             Destroy(gameObject.GetComponent<SpriteRenderer>());
             Destroy(gameObject, audioClip.length);
         }
         else
         {
-            Debug.Log($"{name} collided with {healthSystem.name}");
+            SoundManager.instance.playSound(audioClipHitting, transform, 0.5f);
             healthSystem.DealDamage(damage);
-            Destroy(gameObject);
-            Debug.Log($"The player Health now is {healthSystem.getHealth}");
+            Destroy(gameObject.GetComponent<BoxCollider2D>());
+            Destroy(gameObject.GetComponent<SpriteRenderer>());
+            Destroy(gameObject, audioClipHitting.length);
+
         }
 
     }
