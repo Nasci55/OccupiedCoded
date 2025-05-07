b0b851a08c3e7edd9b1a23e2a2f090a37030465f
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-05-07 09:34:47 +0100

    Add Chase method, the enemy now follows the player

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,16 +1,59 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Player player;
+    private Vector3 playerPos;
+    private float currentVelocity;
+
+
+    [SerializeField]
+    private Vector2 velocity;
+    
+    [SerializeField]
+    private float maxDistance;
+
     void Start()
     {
-        
+        player = FindFirstObjectByType<Player>();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    private void Chase()
     {
-        
+        if (player != null)
+        {
+            if (playerPos.x - transform.position.x < maxDistance)
+            {
+                currentVelocity = 0;
+            }
+            else
+            {
+
+                if (playerPos.x < transform.position.x)
+                {
+                    Debug.Log("Ta a atras");
+                    currentVelocity = -velocity.x;
+                    
+                }
+                else if (playerPos.x > transform.position.x)
+                {
+                    Debug.Log("Ta a frente");
+                    currentVelocity = velocity.x;
+                }
+            }
+            
+            
+            
+            transform.position = new Vector3(transform.position.x + currentVelocity, transform.position.y, transform.position.z);
+
+        }
+    }
+
+    private void Update()
+    {
+        playerPos = player.transform.position;
+        Chase();
     }
 }
