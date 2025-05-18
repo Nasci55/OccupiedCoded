fe7c1760e3631d870e148a782ba7a7d027d78226
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-05-18 15:36:46 +0100

    Add new scripts enemy attack and attack range

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private TAG_EnemyAttackArea TAG_AttackArea;
+    private Collider2D attackCollider;
+    private bool attacking = false;
+
     void Start()
     {
-        
+        TAG_AttackArea = FindFirstObjectByType<TAG_EnemyAttackArea>();
+        attackCollider = TAG_AttackArea.GetComponent<Collider2D>();
+        attackCollider.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Attack()
+    {
+        attacking = true;
+        attackCollider.enabled= attacking;
     }
 }
