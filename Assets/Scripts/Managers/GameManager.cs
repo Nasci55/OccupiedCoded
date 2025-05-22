fbba969cfa23ab96652c6eb10711fe3356987ed1
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-05-22 09:58:10 +0100

    Add new cheat code to skip to the basement

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private Transform destination;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +16,9 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.B) && Input.GetKey(KeyCode.Y) && Input.GetKey(KeyCode.E))
+        {
+            player.position = destination.position;
+        }
     }
 }
