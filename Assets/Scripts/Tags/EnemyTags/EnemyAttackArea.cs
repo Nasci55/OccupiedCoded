3cdfb0a7b37a719d040e5928580fb73857cbb798
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-05-22 14:03:05 +0100

    Fix enemy attack collider

--- Assets/Scripts/Tags/EnemyTags/EnemyAttackArea.cs
+++ Assets/Scripts/Tags/EnemyTags/EnemyAttackArea.cs
@@ -9,18 +9,34 @@ public class EnemyAttackArea : MonoBehaviour
     private GameObject playerObject;
 
     [SerializeField]
-    private string AttackParameter = "Attack"; 
+    private string AttackParameter = "Attack";
+
+    private PlayerHiding PlayerHiding;
+    private bool isPlayerHiding;
     private int damage = 1;
+    private void Start()
+    {
+        PlayerHiding = FindFirstObjectByType<PlayerHiding>();
+        isPlayerHiding = PlayerHiding.currentlyHiding;
+    }
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
         HealthSystem healthSystem = collider.GetComponentInParent<HealthSystem>();
-        if (healthSystem != null)
+        if (isPlayerHiding == false)
         {
-            Debug.Log($"{name} collided with {healthSystem.name}");
-            healthSystem.DealDamage(damage);
-            animator.SetTrigger(AttackParameter);
-            SceneTransition.TransitionToScene("Main Menu Restart");
-            Debug.Log($"The player Health now is {healthSystem.getHealth}");
+            if (healthSystem != null)
+            {
+                Debug.Log($"{name} collided with {healthSystem.name}");
+                healthSystem.DealDamage(damage);
+                animator.SetTrigger(AttackParameter);
+                SceneTransition.TransitionToScene("Main Menu Restart");
+                Debug.Log($"The player Health now is {healthSystem.getHealth}");
+            }
         }
     }
+
+
+
+
 }
