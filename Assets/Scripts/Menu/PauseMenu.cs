b4032f386e7282093c4a7fd8c0940a65e04c7f8d
SimonXPTO <simaodurao@gmail.com>
2025-05-25 01:40:42 +0100

    Add Pause Menu

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pauseMenuUI;
+
+    [SerializeField]
+    private GameObject flashlight;
+
+    private bool isPaused;
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseMenuUI.activeSelf)
+            {
+                OnResumeGame();
+            }
+            else
+            {
+                OnPauseMenu();
+            }
+        }
+    }
+
+    public void OnPauseMenu()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        flashlight.SetActive(false);
+    }
+
+    public void OnResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+        flashlight.SetActive(true);
+    }
+}
